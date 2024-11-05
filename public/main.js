49841e74f6a696ccfe32a51072cf489d0e78b3c4
Corinne GoodSmith <corinnegoodsmith@gmail.com>
2024-11-05 10:43:09 -0600

    updated sort fuctionality

--- public/main.js
+++ public/main.js
@@ -1,81 +1,61 @@
-const deleteText = document.querySelectorAll('.fa-trash')
-const movieForms = document.querySelectorAll('.movieList form');
-
-Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteMovie)
-})
-
-Array.from(movieForms).forEach((form) => {
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        updateRating(this); // Pass the form element to updateRating
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    attachEventListeners();
 });
 
-async function deleteMovie(){
-    const mName = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteMovie', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'movieName': mName
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
+async function fetchRequest(url, method, body) {
+    try {
+        const response = await fetch(url, {
+            method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+        });
+        return response.ok ? response.json() : console.error('Request failed:', response.status);
+    } catch (error) {
+        console.error(error);
     }
+}
+
+function attachEventListeners() {
+    document.querySelectorAll('.fa-trash').forEach(element =>
+        element.addEventListener('click', deleteMovie)
+    );
+
+    document.querySelectorAll('.movieList form').forEach(form =>
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            updateRating(this);
+        })
+    );
+}
+
+async function deleteMovie() {
+    const mName = this.closest('.movieList').querySelector('.movie').innerText;
+    const data = await fetchRequest('/deleteMovie', 'DELETE', { movieName: mName });
+    if (data) location.reload();
 }
 
 async function updateRating(formElement) {
     const movie = formElement.closest('.movieList').querySelector('.movie').innerText;
     const rating = formElement.querySelector('select[name="updateRating"]').value;
 
-    if (rating === "null") {
-        alert("Please select a valid rating.");
+    if (rating === 'null') {
+        alert('Please select a valid rating.');
         return;
     }
 
-    try {
-        const response = await fetch('/updateMovieRating', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                mName: movie,
-                mRating: rating
-            })
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data.message);
-            if (data.movies) {
-                updateMoviesListInDOM(data.movies);
-            }
-        } else {
-            console.log('Failed to update rating:', response.status);
-        }
-    } catch (err) {
-        console.log(err);
-    }
+    const data = await fetchRequest('/updateMovieRating', 'PUT', { mName: movie, mRating: rating });
+    if (data?.movies) updateMoviesListInDOM(data.movies);
 }
 
 function updateMoviesListInDOM(movies) {
     const movieListContainer = document.querySelector('.movies');
-    movieListContainer.innerHTML = ''; 
-
-    movies.forEach(movie => {
-        const movieItem = document.createElement('li');
-        movieItem.classList.add('movieList');
-        movieItem.innerHTML = `
+    movieListContainer.innerHTML = movies.map(movie => `
+        <li class="movieList">
             <span class="movie">${movie.movieName}</span>
             <span class="rating">${movie.movieRating}</span>
-            <span><form action="/updateMovieRating" method="PUT">
-            <select name="updateRating" >
+            <span>
+                <form action="/updateMovieRating" method="PUT">
+                    <select name="updateRating">
                         <option value="null">Choose Rating</option>
                         <option value="A+">A+</option>
                         <option value="A">A</option>
@@ -89,28 +69,11 @@ function updateMoviesListInDOM(movies) {
                         <option value="D">D</option>
                         <option value="F">F</option>
                     </select>
-                    <input type="submit">
-                </form></span>
-                <span class='fa fa-trash'></span>
-        `;
-        movieListContainer.appendChild(movieItem);
-    });
-
+                    <input type="submit" value="Update">
+                </form>
+            </span>
+            <span class='fa fa-trash'></span>
+        </li>
+    `).join('');
     attachEventListeners();
-}
-
-function attachEventListeners() {
-    const deleteText = document.querySelectorAll('.fa-trash');
-    const movieForms = document.querySelectorAll('.movieList form');
-
-    Array.from(deleteText).forEach((element) => {
-        element.addEventListener('click', deleteMovie);
-    });
-
-    Array.from(movieForms).forEach((form) => {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-            updateRating(this); // Pass the form element to updateRating
-        });
-    });
 }
